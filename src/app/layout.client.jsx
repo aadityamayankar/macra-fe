280cf2b78a1505fc97d5dbd8a4af7420228d22a0
Mayankar <53344662+aadityamayankar@users.noreply.github.com>
2024-12-31 16:07:36 +0530

    added favicon
    * added favicon
    
    * remove use client directive from layout
    
    * create a separate client and server layout

---
+++ src/app/layout.client.jsx
@@ -0,0 +1,51 @@
+'use client';
+import { useEffect } from 'react';
+import Header from "@/components/fe/Header";
+import { Provider } from "@/components/ui/provider";
+import Footer from "@/components/fe/Footer";
+import { Box, Flex } from "@chakra-ui/react";
+import { Quicksand } from "next/font/google";
+import useStore from '@/store/useStore';
+import { validateSession, fetchUserProfile } from '@/api';
+import { Analytics } from '@vercel/analytics';
+
+const quicksand = Quicksand({ subsets: ["latin"], display: "swap", weight: '600' });
+
+export default function RootLayout({ children }) {
+  const setIsAuthenticated = useStore((state) => state.setIsAuthenticated);
+  const setUserProfile = useStore((state) => state.setUserProfile);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      try {
+        const isAuthenticated = await validateSession();
+        setIsAuthenticated(isAuthenticated);
+
+        if (isAuthenticated) {
+          const userProfile = await fetchUserProfile();
+          setUserProfile(userProfile);
+        }
+      } catch (error) {
+        console.error('Failed to check session or fetch user profile:', error);
+      }
+    };
+
+    checkSession();
+  }, [setIsAuthenticated, setUserProfile]);
+
+  return (
+    <html lang="en" suppressHydrationWarning className={quicksand.className}>
+      <body>
+        <Provider>
+          <Flex direction="column" minHeight="100vh">
+            <Header />
+            <Box as="main" flex="1">
+              {children}
+            </Box>
+            <Footer />
+          </Flex>
+        </Provider>
+      </body>
+    </html>
+  );
+}
